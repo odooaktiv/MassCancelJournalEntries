d8542a9bebd350245a91836544e6f24068768f5a
AktivSoftware <odoo@aktivsoftware.com>
2019-11-18 15:41:47 +0530

    [ADD]added license.

--- mass_cancel_journal_entries/__manifest__.py
+++ mass_cancel_journal_entries/__manifest__.py
@@ -7,6 +7,7 @@
     'website': "http://www.aktivsoftware.com",
     'category': 'Invoicing',
     'version': '12.0.1.0.1',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['account_cancel', 'account'],
